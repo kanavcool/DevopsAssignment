0175ecc83d150b9649d9b9054ce02e688e78dd3e
kanavcool <kanavstudy44@gmail.com>
2025-07-03 04:33:04 +0530

    CRM Framework script 03-07-2025

---
+++ src/test/java/com/CRM/TestCase/BaseClass.java
@@ -0,0 +1,5 @@
+package com.CRM.TestCase;
+
+public class BaseClass {
+
+}
---
+++ src/test/java/com/CRM/TestCase/HomePageTestCase.java
@@ -0,0 +1,9 @@
+package com.CRM.TestCase;
+
+import org.testng.annotations.Test;
+
+public class HomePageTestCase {
+  @Test
+  public void f() {
+  }
+}
